9fe1121142c6eb68c0f494effc79070e473519a9
kblandonr <kblandonr@ucenfotec.ac.cr>
2024-11-13 13:07:33 -0600

    Commit

---
+++ ManejoUsuarios/Models/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ManejoUsuarios.Models;
+
+[Table("Usuarios")]
+public partial class Usuario
+{
+    public string? Nombre { get; set; }
+
+    public string? Apellido { get; set; }
+
+    public int? Edad { get; set; }
+    [Key]
+    public int Id { get; set; }
+}
